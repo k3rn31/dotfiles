282ae97da833f9f6c7521a8ff45f569272cd1c2c
Davide Petilli <davide.petilli@gmail.com>
2019-12-30 11:49:06 +0100

    Add YAML mode

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -16,8 +16,7 @@
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
    (quote
-    (helm markdown-mode ## evil-magit magit evil-org evil kaolin-themes auto-compile use-package)))
- '(scheme-program-name "mit-scheme"))
+    (evil-magit yaml-mode markdown-mode clojure-mode-extra-font-locking cider clojure-mode paredit rainbow-delimiters magit evil-org evil-collection evil helm zenburn-theme auto-compile use-package))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
