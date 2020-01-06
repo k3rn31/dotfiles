72c5401cd3b22f1e482a993e86b2c80f7ad27507
Davide Petilli <davide.petilli@gmail.com>
2020-01-06 08:56:49 +0100

    Change st installation to builds

--- setup.sh
+++ setup.sh
@@ -63,7 +63,7 @@ case $option in
     ln -sfnv "$PWD/.config/picom" ~/.config/picom
     ln -sfnv "$PWD/.config/qutebrowser" ~/.config/qutebrowser
     ln -sfnv "$PWD/.config/zathura" ~/.config/zathura
-    ln -sfnv "$PWD/st" ~/.config/st
+    ln -sfnv "$PWD/st" ~/builds/st
     ln -sfnv "$PWD/.weechat" ~/.weechat
     ;;
 
