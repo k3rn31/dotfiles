ae79c10e624cdb4b0d4d0f9972da4543ee533d74
Davide Petilli <davide.petilli@gmail.com>
2020-01-11 08:14:22 +0100

    Move Custom settings in a specific file

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -4,22 +4,6 @@
 ;; This is the first thing that gets loaded
 ;;
 
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(org-export-backends (quote (ascii html icalendar latex md odt confluence)))
- '(package-selected-packages
-   (quote
-    (diff-hl visual-fill-column feature-mode evil-magit yaml-mode markdown-mode clojure-mode-extra-font-locking cider clojure-mode paredit rainbow-delimiters magit evil-org evil-collection evil helm zenburn-theme auto-compile use-package))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
-
 ;; Set a variable holding the current configuration directory
 (setq config-dir (file-name-directory
                   (or load-file-name (buffer-file-name))))
